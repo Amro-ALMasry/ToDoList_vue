adcda88bc16aeb0e8d6cd7817c6878cc56a071b3
AMRO AL-MASRY <amro.al.masry999@gmail.com>
2025-04-27 23:47:31 +0200

    sistemare pag rigistra

--- src/views/registra.vue
+++ src/views/registra.vue
@@ -4,8 +4,16 @@ import {useAuthStore} from "../stores/authStore.js"
 const authStore = useAuthStore() 
 const email = ref("");
 const password = ref("");
+const ConfermaPassword = ref("");
 const showPassword = ref(false);
-
+const registra = async() =>{
+  if (password.value == ConfermaPassword.value) {
+    await authStore.register(email.value,password.value)
+  }else{
+    alert("Le password non coincidono")
+  }
+  
+}
 const togglePasswordVisibility = () => {
   showPassword.value = !showPassword.value;
 };
@@ -30,7 +38,7 @@ const togglePasswordVisibility = () => {
         </svg>
       </span>
     </div>
-    
+    <!-- inserisci password -->
     <div class="input-group">
       <input 
         :type="showPassword ? 'text' : 'password'" 
@@ -57,7 +65,35 @@ const togglePasswordVisibility = () => {
         </svg>
       </span>
     </div>
-    <input type="button" @click="authStore.register(email,password)" class="register-button" value="Registrati">
+
+    <!-- conferma password -->
+    <div class="input-group">
+      <input 
+        :type="showPassword ? 'text' : 'password'" 
+        v-model="ConfermaPassword" 
+        placeholder=" " 
+        class="form-input"
+      >
+      <label class="input-label">Conferma la password</label>
+      <span class="input-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+          <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+        </svg>
+      </span>
+      <span class="password-toggle" @click="togglePasswordVisibility">
+        <svg v-if="showPassword" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+            <circle cx="12" cy="12" r="3"></circle>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          
+          <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+          <line x1="1" y1="1" x2="23" y2="23"></line>
+        </svg>
+      </span>
+    </div>
+    <input type="button" @click="registra" class="register-button" value="Registrati">
     
     <div class="login-link">
       Hai già un account? <router-link to="login">Accedi</router-link>
